2e1dbb9399ea48d4a295b24c7277de893261d254
aufakamal <aufakamal9506@gmail.com>
2026-06-19 16:03:00 +0800

    Login Register php, css, js update

--- login_register/login.php
+++ login_register/login.php
@@ -14,9 +14,9 @@
     <div class="main">
 
         <div class="left-sec">
-            <a href="mainPage.php" class="back">
+            <!-- <a href="mainPage.php" class="back">
                 <img src="loginRegisterImage/backIconDark.png" alt="Back">
-            </a>
+            </a> -->
 
             <h2>Welcome To <br>
             UTeM's PKU <br>
